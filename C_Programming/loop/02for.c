df6475622f1f2a4a92f45f072fb86a83c14f993e
shikha <shikhakumari3039@gmail.com>
2024-02-07 23:03:40 +0530

    if else

---
+++ C_Programming/loop/02for.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// int main(){
+//     int i;
+//     printf("Enter the number:");
+//     for(i=1; i<11; i++){
+//         printf("%d",i);
+//     }
+// }
+
+// int main(){
+//     int i, n, sum=0;
+//     printf("Enter the numbers:");
+//     scanf("%d",&n);
+//     for(i=1; i<=n; i++){
+//         sum+=i;
+//     }
+//     printf("sum =%d",sum);
+// }
+
