27918d76558b41a6ce44401d7a67c00e093aa3b7
shikha <shikhakumari3039@gmail.com>
2024-03-01 22:20:03 +0530

    Loop

--- C_Programming/loop/while.c
+++ C_Programming/loop/while.c
@@ -8,22 +8,22 @@
 //     }   
 // }
 
-int main(){
-    int a;
-    int bits;
-    scanf("%d",&bits);
-    while(1){
-        if(bits ==-1){
-            break;
-        }
-        if(bits ==1){
-            a++;
-        }
-    }
-    if(a%2==0){
-        printf("1");
-    }
-    else{
-        printf("0");
-    }
-}
+// int main(){
+//     int a;
+//     int bits;
+//     scanf("%d",&bits);
+//     while(1){
+//         if(bits ==-1){
+//             break;
+//         }
+//         if(bits ==1){
+//             a++;
+//         }
+//     }
+//     if(a%2==0){
+//         printf("1");
+//     }
+//     else{
+//         printf("0");
+//     }
+// }
