b10b23fcf605d1436b287bc654ff0da436508c8c
shikha <shikhakumari3039@gmail.com>
2025-07-16 14:05:46 +0530

    Training_class

--- Training_class/Day_9.c
+++ Training_class/Day_9.c
@@ -131,18 +131,79 @@
 
 // wap in built string function using function 
 //1
-#include <stdio.h>
-#include <string.h>
-int main() {
-    char str1[100], str2[100];
-    printf("Enter a string1: ");
-    gets(str1);
-    printf("Enter a string2: ");
-    gets(str2); 
-    //printf("Length =%d\n", strlen(str));
-    //printf("Length= %s\n", strrev(str)); 
-    //printf("Length= %d\n", strlwr(str)); 
-    //printf("Length= %d\n", strupr(str)); 
-    //printf("Copied string = %s\n", strcpy(str1, str2)); 
-    printf("Compare string= %d\n", strcmp(str1, str2)); 
-}
+// #include <stdio.h>
+// #include <string.h>
+// int main() {
+//     char str1[100], str2[100];
+//     printf("Enter a string1: ");
+//     gets(str1);
+//     printf("Enter a string2: ");
+//     gets(str2); 
+//     printf("Length =%d\n", strlen(str));
+//     printf("Length= %s\n", strrev(str)); 
+//     printf("Length= %d\n", strlwr(str)); 
+//     printf("Length= %d\n", strupr(str)); 
+//     printf("Copied string = %s\n", strcpy(str1, str2)); 
+//     printf("Compare string= %d\n", strcmp(str1, str2)); // ye de raha h asscii ki values
+// }
+
+// New topics
+// wap to passing 1d array into a function
+// #include<stdio.h>
+// void display(int a[], int n){
+//     int i;
+//     printf("Arrays elements are: ");
+//     for(i=0; i<n; i++){
+//         printf("%d", a[i]);
+//     }
+// }
+// int main(){
+//     int a[100], i, n;
+//     printf("Enter the value of n: ");
+//     scanf("%d", &n);
+//     printf("Enter the elements of array: ");
+//     for(i=0; i<n; i++){
+//         scanf("%d", &a[i]);
+//     }
+//     display(a,n);             // base array or size
+// }
+
+// wap to passing 2d array into a function
+// #include <stdio.h>
+// void display(int a[100][100], int r, int c) {
+//     int i, j;
+//     printf("Array elements are:\n");
+//     for (i = 0; i < r; i++) {
+//         for (j = 0; j < c; j++) {
+//             printf("%d\t", a[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
+// int main() {
+//     int a[100][100], i, j, r, c;
+//     printf("Enter the size of row and column: ");
+//     scanf("%d%d", &r, &c);
+//     printf("Enter the elements of array:\n");
+//     for (i = 0; i < r; i++) {
+//         for (j = 0; j < c; j++) {
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+//     display(a, r, c);
+// }
+
+// wap to passing character into a function
+// #include <stdio.h>
+// void displayChar(char ch) {
+//     printf("You entered character: %c\n", ch);
+// }
+// int main() {
+//     char c;
+//     printf("Enter a character: ");
+//     scanf(" %c", &c); 
+//     displayChar(c);
+// }
+
+//TOPIC IS STRUCTURE
+
